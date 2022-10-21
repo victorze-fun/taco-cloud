0e722fe132679c1b749f95ca78c6a218d04a080d
Victor Hugo <victorze@protonmail.com>
2022-10-21 16:35:18 -0500

    Remove tests

--- src/main/java/com/victorze/tacocloud/controllers/DesignTacoController.java
+++ src/main/java/com/victorze/tacocloud/controllers/DesignTacoController.java
@@ -33,6 +33,7 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/design")
 @SessionAttributes("order")
 public class DesignTacoController {
+    
     private final IngredientRepository ingredientRepo;
 
     private TacoRepository tacoRepo;
